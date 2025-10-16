51b3dd35627eca078a6880fd05abf7cb6a8be3b0
mtc98 <mtc98tw@gmail.com>
2025-10-16 14:48:14 +0800

    feat:新增 Vite 設定 修正 路徑

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // 這裡是最關鍵的設定！
+  // 請將 '/chat1/' 換成您的 GitHub 倉庫名稱
+  base: '/chat1/'
 })
